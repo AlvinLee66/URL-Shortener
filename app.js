23e95dbbfa95a822e52071a6e5b01237fe73aaf0
AlvinLee66 <ewqaz001@gmail.com>
2022-04-21 12:33:11 +0800

    modify index.hbs style and app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
-const exphbs = require('express-handlebars')
 const Shorten = require('./models/shorten')
 const generateShortenCode = require('./untis/generateShortenCode')
 const shortenCodeLength = 5
+
 require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
@@ -16,16 +18,14 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  const url = req.body.url.trim()
+  const url = req.body.url.trim() // 除去前後因使用者不小心輸入的空格
+  const shortenCode = generateShortenCode(shortenCodeLength)
 
   Shorten.find({ url: url })
     .lean()
     .then(result => {
       if (result.length === 0) { // 如果為0，表示資料庫沒有
-        const newShorten = {
-          url: url,
-          shortenCode: generateShortenCode(shortenCodeLength)
-        }
+        const newShorten = { url, shortenCode }
         Shorten.create(newShorten) // 建立新資料
         res.render('index', { shorten: newShorten }) // 傳給 index 去渲染得到的短網址
       } else {
@@ -36,13 +36,14 @@ app.post('/', (req, res) => {
 })
 
 app.get('/:shortenCode', (req, res) => {
-  const shortenCode = req.params.shortenCode.trim()
-
-  Shorten.find({ shortenCode: shortenCode })
+  const shortenCode = req.params.shortenCode.trim() // 如果使用者不小心在前後輸入空格一樣可以轉到對應的網頁
+  if (shortenCode.length !== 5) return res.redirect('/') // 如果輸入的短網址不是5碼則一律回到首頁
+  
+  Shorten.find({ shortenCode }) // 用 shortenCode 找出對應的資料
     .lean()
     .then(result => {
       const url = result[0].url
-      res.redirect(`${url}`)
+      res.redirect(`${url}`) // 導入對應的原網址
     })
     .catch(err => console.log(err))
 })
