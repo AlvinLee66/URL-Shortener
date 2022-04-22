1342e3bf32ab34fb89491a8534c59f82894ad6c2
AlvinLee66 <ewqaz001@gmail.com>
2022-04-22 11:35:13 +0800

    modify generateShortenCode.js

--- untis/generateShortenCode.js
+++ untis/generateShortenCode.js
@@ -1,11 +1,12 @@
 function generateShortenCode(shortenCodeLength) {
+  const codeLength = shortenCodeLength
   const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
   const upperCaseLetters = lowerCaseLetters.toUpperCase()
   const numbers = '0123456789'
   const collection = (lowerCaseLetters + upperCaseLetters + numbers).split('')
   
   let shortenCode = ''
-  for (i = 0; i < Number(shortenCodeLength); i++) {
+  for (i = 0; i < Number(codeLength); i++) {
     shortenCode += chooseRandomly(collection)
   }
 
